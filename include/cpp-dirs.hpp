8440c4d19c2981ba312f2fa6be5b17fe042d9bb1
Maou-Shimazu <eldaddanladi@gmail.com>
2022-06-12 05:49:45 -0600

    windows dirs

--- include/cpp-dirs.hpp
+++ include/cpp-dirs.hpp
@@ -20,14 +20,34 @@ namespace dirs {
     #endif
 
     #if defined(WIN32) || defined(_WIN64)
-    char* hn = std::getenv("COMPUTERNAME");
-    char* un = std::getenv("USERNAME");
-
+    const char* hn = std::getenv("COMPUTERNAME");
+    const char* un = std::getenv("USERNAME");
+    const char* h = std::getenv("USERPROFILE");
+    const char* temp = std::getenv("TEMP");
+    const char* dd = std::getenv("APPDATA");
+    const char* ldd = std::getenv("LOCALAPPDATA");
+    const char* pub = std::getenv("PUBLIC");
+    std::string v = std::string(h) + "\\Videos";
+    std::string doc = std::string(h) + "\\Documents";
+    std::string pic = std::string(h) + "\\Pictures";
+    std::string down = std::string(h) + "\\Downloads";
+    std::string desk = std::string(h) + "\\Desktop";
+    std::string aud = std::string(h) + "\\Music";
     #endif
 
     const std::string hostname = std::string(hn);
     const std::string username = std::string(un);
-
+    const std::string home = std::string(h);
+    const std::string cache = std::string(temp);
+    const std::string data_dir = std::string(dd);
+    const std::string data_local_dir = std::string(ldd);
+    const std::string public_dir = std::string(pub);
+    const std::string video = v;
+    const std::string documents = doc;
+    const std::string pictures = pic;
+    const std::string downloads = down;
+    const std::string desktop = desk;
+    const std::string audio = aud;
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,16 @@
 int main() {
     std::cout << dirs::hostname << std::endl;
     std::cout << dirs::username << std::endl;
+    std::cout << dirs::home << std::endl;
+    std::cout << dirs::cache << std::endl;
+    std::cout << dirs::data_dir << std::endl;
+    std::cout << dirs::data_local_dir << std::endl;
+    std::cout << dirs::video << std::endl;
+    std::cout << dirs::documents << std::endl;
+    std::cout << dirs::pictures << std::endl;
+    std::cout << dirs::downloads << std::endl;
+    std::cout << dirs::desktop << std::endl;
+    std::cout << dirs::public_dir << std::endl;
+    std::cout << dirs::audio << std::endl;
     return 0;
 }
