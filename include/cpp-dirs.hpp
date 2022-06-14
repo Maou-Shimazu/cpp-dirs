c57916e586374a783c07f32008a8488a127f4aa5
Maou-Shimazu <eldaddanladi@gmail.com>
2022-06-14 04:20:43 -0600

    class visibility

--- include/cpp-dirs.hpp
+++ include/cpp-dirs.hpp
@@ -3,53 +3,113 @@
 #ifndef CPP_DIRS_HPP
 #define CPP_DIRS_HPP
 
-#include <iostream>
 #include <cstdlib>
+#include <filesystem>
+#include <iostream>
 
-namespace dirs {
-    #ifdef unix
+#ifdef unix
+#include <limits.h>
+#include <unistd.h>
+#endif
 
-    #include <limits.h>
-    #include <unistd.h>
-
+std::string empty_dir(std::string dir) {
+    auto dir_exists = [](const auto &file) {
+        return std::filesystem::is_directory(file) ? true : false;
+    };
+    if (dir_exists(dir))
+        return dir;
+    else
+        return "";
+}
+// note: add overhead comments for each func
+class dirs {
+  private:
+#ifdef unix
     char hn[HOST_NAME_MAX];
     char un[LOGIN_NAME_MAX];
     gethostname(hostname, HOST_NAME_MAX);
     getlogin_r(username, LOGIN_NAME_MAX);
 
-    #endif
+#endif
 
-    #if defined(WIN32) || defined(_WIN64)
-    const char* hn = std::getenv("COMPUTERNAME");
-    const char* un = std::getenv("USERNAME");
-    const char* h = std::getenv("USERPROFILE");
-    const char* temp = std::getenv("TEMP");
-    const char* dd = std::getenv("APPDATA");
-    const char* ldd = std::getenv("LOCALAPPDATA");
-    const char* pub = std::getenv("PUBLIC");
+#if defined(WIN32) || defined(_WIN64)
+    const char *hn = std::getenv("COMPUTERNAME");
+    const char *un = std::getenv("USERNAME");
+    const char *h = std::getenv("USERPROFILE");
+    const char *temp = std::getenv("TEMP");
+    const char *dd = std::getenv("APPDATA");
+    const char *ldd = std::getenv("LOCALAPPDATA");
+    const char *pub = std::getenv("PUBLIC");
     std::string v = std::string(h) + "\\Videos";
     std::string doc = std::string(h) + "\\Documents";
     std::string pic = std::string(h) + "\\Pictures";
     std::string down = std::string(h) + "\\Downloads";
     std::string desk = std::string(h) + "\\Desktop";
     std::string aud = std::string(h) + "\\Music";
-    #endif
+#endif
 
+  public:
     const std::string hostname = std::string(hn);
     const std::string username = std::string(un);
-    const std::string home = std::string(h);
-    const std::string cache = std::string(temp);
-    const std::string data_dir = std::string(dd);
-    const std::string data_local_dir = std::string(ldd);
-    const std::string public_dir = std::string(pub);
-    const std::string video = v;
-    const std::string documents = doc;
-    const std::string pictures = pic;
-    const std::string downloads = down;
-    const std::string desktop = desk;
-    const std::string audio = aud;
-}
 
+  public:
+    dirs() = default;
+    static const std::string home() {
+        dirs d;
+        const std::string home = std::string(d.h);
+        return empty_dir(home);
+    }
+    static const std::string cache() {
+        dirs d;
+        const std::string cache = std::string(d.temp);
+        return empty_dir(cache);
+    }
+    static const std::string data_dir() {
+        dirs d;
+        const std::string data_dir = std::string(d.dd);
+        return empty_dir(data_dir);
+    }
+    static const std::string data_local_dir() {
+        dirs d;
+        const std::string data_local_dir = std::string(d.ldd);
+        return empty_dir(data_local_dir);
+    }
+    static const std::string video() {
+        dirs d;
+        const std::string video = d.v;
+        return empty_dir(video);
+    }
+    static const std::string documents() {
+        dirs d;
+        const std::string documents = d.doc;
+        return empty_dir(documents);
+    }
+    static const std::string pictures() {
+        dirs d;
+        const std::string pictures = d.pic;
+        return empty_dir(pictures);
+    }
+    static const std::string downloads() {
+        dirs d;
+        const std::string downloads = d.down;
+        return empty_dir(downloads);
+    }
+    static const std::string desktop() {
+        dirs d;
+        const std::string desktop = d.desk;
+        return empty_dir(desktop);
+    }
 
+    static const std::string audio() {
+        dirs d;
+        const std::string audio = d.aud;
+        return empty_dir(audio);
+    }
 
+    static const std::string public_dir() {
+        dirs d;
+        const std::string public_dir = std::string(d.pub);
+        return empty_dir(public_dir);
+    }
+};
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,19 @@
 #include "cpp-dirs.hpp"
 
 int main() {
-    std::cout << dirs::hostname << std::endl;
-    std::cout << dirs::username << std::endl;
-    std::cout << dirs::home << std::endl;
-    std::cout << dirs::cache << std::endl;
-    std::cout << dirs::data_dir << std::endl;
-    std::cout << dirs::data_local_dir << std::endl;
-    std::cout << dirs::video << std::endl;
-    std::cout << dirs::documents << std::endl;
-    std::cout << dirs::pictures << std::endl;
-    std::cout << dirs::downloads << std::endl;
-    std::cout << dirs::desktop << std::endl;
-    std::cout << dirs::public_dir << std::endl;
-    std::cout << dirs::audio << std::endl;
+    dirs host;
+    std::cout << host.hostname << std::endl;
+    std::cout << host.username << std::endl;
+    std::cout << dirs::home() << std::endl;
+    std::cout << dirs::cache() << std::endl;
+    std::cout << dirs::data_dir() << std::endl;
+    std::cout << dirs::data_local_dir() << std::endl;
+    std::cout << dirs::video() << std::endl;
+    std::cout << dirs::documents() << std::endl;
+    std::cout << dirs::pictures() << std::endl;
+    std::cout << dirs::downloads() << std::endl;
+    std::cout << dirs::desktop() << std::endl;
+    std::cout << dirs::public_dir() << std::endl;
+    std::cout << dirs::audio() << std::endl;
     return 0;
 }
